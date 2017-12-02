b6a2eaffde8adf10a0e1273346ab73e452d6e22c
Nishant Sheikh <nsheikh2@illinois.edu>
2017-12-02 17:26:15 -0600

    Added some Javadoc

---
+++ src/com/whatever/gameoflife/Main.java
@@ -0,0 +1,16 @@
+package com.whatever.gameoflife;
+
+/**
+ * The main class for the program.
+ * @author nshei
+ *
+ */
+public class Main {
+    /**
+     * The entry point for the program.
+     * @param args command-line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
