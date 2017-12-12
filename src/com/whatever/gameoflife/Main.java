34865939cc0cf4b39b2aef12d5decac0f7feefa1
Nishant Sheikh <nsheikh2@illinois.edu>
2017-12-11 21:42:07 -0600

    Fixed more ugly spacing issues I think

--- src/com/whatever/gameoflife/Main.java
+++ src/com/whatever/gameoflife/Main.java
@@ -7,46 +7,46 @@ import javax.swing.JFrame;
 
 public class Main {
 
-	public static void main(String[] args)
-	{
-	    //Set up components, reasonable defaults
-	    GameCanvas gc = new GameCanvas(400, 400, 10);
-	    gc.setTimeStep(100);
-	    gc.startGame();
-
-	    //window setup
-	    JFrame frame = new JFrame();
-	    frame.setResizable(true);
-	    frame.setTitle("Game of Life");
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.requestFocus();
+    public static void main(String[] args)
+    {
+	//Set up components, reasonable defaults
+	GameCanvas gc = new GameCanvas(400, 400, 10);
+	gc.setTimeStep(100);
+	gc.startGame();
+        
+        //window setup
+	JFrame frame = new JFrame();
+        frame.setResizable(true);
+        frame.setTitle("Game of Life");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.requestFocus();
         frame.addKeyListener(new KeyListener() {
             @Override
-            public void keyPressed(KeyEvent e) {
-                switch(e.getKeyChar()) {
-                case ' ':
-                    gc.togglePause();
-                    break;
-                case 'r':
-                    gc.randomize((float) Math.random());
-                    break;
-                case 'c':
-                    gc.clearAll();
-                default:
-                    break;
+                public void keyPressed(KeyEvent e) {
+                    switch(e.getKeyChar()) {
+                    case ' ':
+                        gc.togglePause();
+                        break;
+                    case 'r':
+                        gc.randomize((float) Math.random());
+                        break;
+                    case 'c':
+                        gc.clearAll();
+                    default:
+                        break;
+                    }
                 }
-            }
             @Override
             public void keyReleased(KeyEvent arg0) { }
             @Override
             public void keyTyped(KeyEvent arg0) { }
         });
 
-	    //Add components
-	    frame.getContentPane().add(gc);
+        //Add components
+        frame.getContentPane().add(gc);
 
-	    //show frame
-	    frame.pack();
-	    frame.setVisible(true);
-	}
+        //show frame
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
